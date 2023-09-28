360e475728223dbd419f839b48b6c4a636d5132f
Karuitha <diakingathia2005@gmail.com>
2023-09-28 15:53:14 +0300

    Added R script for dark themes dark_themes.R

---
+++ dark_themes.R
@@ -0,0 +1,24 @@
+## Load packages manager ----
+if(!require("pacman")){
+  install.packages("pacman")
+}
+
+# Load R packages ----
+p_load(tidyverse, tidymodels,
+       skimr, GGally, naniar,
+       Amelia, janitor, 
+       remotes, devtools,
+       flextable, kableExtra)
+
+
+# Install the pseudo-package from this Github repository ----
+devtools::install_github("max-alletsee/rstudio-themes")
+
+library(rstudiothemes) # ... then load the library ----
+
+# Example 1: bulk-install all light themes
+install_rstudio_themes(theme = "all_light")
+
+# Example 1: bulk-install all dark themes
+install_rstudio_themes(theme = "all_dark")
+
