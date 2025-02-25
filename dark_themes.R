423dc002d3e626c58daf6469f66a2e7b54fa685a
John King'athia Karuitha <65982152+Karuitha@users.noreply.github.com>
2025-02-25 14:42:02 +0200

    Update dark_themes.R
    Added settings for the static V8 package for R

--- dark_themes.R
+++ dark_themes.R
@@ -11,6 +11,9 @@ p_load(tidyverse, tidymodels,
        flextable, kableExtra,
        ggthemes)
 
+Sys.setenv(DOWNLOAD_STATIC_LIBV8=1)
+install.packages("V8")
+
 # Install the pseudo-package from this Github repository ----
 devtools::install_github("max-alletsee/rstudio-themes")
 
